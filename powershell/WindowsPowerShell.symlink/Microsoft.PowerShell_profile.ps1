b4c602166ffdcd24a6fb627d1c814e014b693fb1
Daniel Dabrowski <dabrowski.daniel@42n.pl>
2016-10-16 17:52:05 +0200

    PS: Chocolatey support

--- powershell/WindowsPowerShell.symlink/Microsoft.PowerShell_profile.ps1
+++ powershell/WindowsPowerShell.symlink/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,9 @@ if (Test-Path $myProfile) {
 . $myProfile
 }
 Remove-Item variable:myProfile
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
